1fecb0fc4a55b6f0fcf61ed2a16b0614621c9359
Catherine Jean <catherinejeanbean@gmail.com>
2017-07-31 16:43:30 -0500

    DS_Store issue

--- ajax-checkpoint-challenge-master/ajax-description.md
+++
@@ -1,6 +0,0 @@
-# What is AJAX?
-Your answer here.
-
-
-# How to Make an AJAX Call
-Your answer here.
--- ajax-checkpoint-challenge-master/app/models/horse.rb
+++
@@ -1,6 +0,0 @@
-class Horse < ApplicationRecord
-
-  validates :name, presence: true
-  validates :age, presence: true
-  validates :breed, presence: true
-end
